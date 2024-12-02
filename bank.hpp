d162a4334c46d7ef0b189ded27967ca4f44f4207
AlexVicente <alex.vicente@sensustec.com.br>
2024-12-02 18:04:47 -0300

    Verificando se existe o investment com o id para vender

--- bank.hpp
+++ bank.hpp
@@ -45,6 +45,8 @@ string buy_investment(BankState &bank_state, string buyer, int amount) {
 
 string sell_investment(BankState &bank_state, string seller,
                        int investment_id) {
+  if (bank_state.investments.find(investment_id) == bank_state.investments.end()) 
+    return "No investment with this id";
   bank_state.balances[seller] += bank_state.investments[investment_id].amount;
   return "";
 }
