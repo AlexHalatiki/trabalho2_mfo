31b3150eac31416a931cce6294992426d36124f3
AlexVicente <alex.vicente@sensustec.com.br>
2024-12-02 18:32:54 -0300

    Verificando se tem saldo para fazer transferencia

--- bank.hpp
+++ bank.hpp
@@ -29,6 +29,8 @@ string withdraw(BankState &bank_state, string withdrawer, int amount) {
 
 string transfer(BankState &bank_state, string sender, string receiver,
                 int amount) {
+  if (amount <= 0)
+    return "Amount should be greater than zero";
   if (bank_state.balances[sender] < amount)
     return "Balance is too low";
   bank_state.balances[sender] -= amount;
